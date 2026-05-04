0dc32a45f20b94708b963f5704a3254cc1a3121c
matprime <matt@mattprimeonline.com>
2026-05-04 22:01:30 +0200

    GTP-5.3 Fix ESLint v9 flat config compatibility

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,14 @@
-import next from "eslint-config-next";
+import { FlatCompat } from "@eslint/eslintrc";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
 
 export default [
-  ...next(),
+  ...compat.extends("next/core-web-vitals"),
 ];
